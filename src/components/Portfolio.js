42eacbafef87230fa9c066001bc5b63fd6c766ee
hmanjun <harshith.hm03@gmail.com>
2022-08-25 20:06:29 -0700

    Add 6 projects to portfolio section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,10 +9,30 @@ export default function Portfolio() {
              repo="https://github.com/hmanjun/City-Bus-System" 
              deployed="https://t12-bus-system.herokuapp.com/" />
 
-            <Card title="City Bus System"
-             img="/images/bus-system.PNG"
-             repo="https://github.com/hmanjun/City-Bus-System" 
-             deployed="https://t12-bus-system.herokuapp.com/" />
+            <Card title="Social Media API"
+             img="/images/social-media.png"
+             repo="https://github.com/hmanjun/social-media-api" 
+             deployed="https://youtu.be/PItJlGoTRtc" />
+
+            <Card title="E-commerce Site Database"
+             img="/images/ecommerce-site.png"
+             repo="https://github.com/hmanjun/ecommerce-site-database" 
+             deployed="https://youtu.be/W6meEHUvbvI" />
+
+            <Card title="Team Profile Storer"
+             img="/images/team-profile.png"
+             repo="https://github.com/hmanjun/Team-Profile-Storer" 
+             deployed="https://youtu.be/7UT0epiHRB0" />
+            
+            <Card title="Dynamic Cookbook"
+             img="/images/dynamic-cookbook.png"
+             repo="https://github.com/hmanjun/dynamic-cookbook" 
+             deployed="https://alenambo02.github.io/dynamic-cookbook/" />
+
+            <Card title="Weather Tracker"
+             img="/images/weather-tracker.png"
+             repo="https://github.com/hmanjun/weather-tracker" 
+             deployed="https://hmanjun.github.io/weather-tracker/" />
         </div>
     )
 }
